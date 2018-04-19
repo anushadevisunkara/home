dfc0c23c6fa34f0122f95867b04b900fef838c03
anusha <anushadevi.sunkara@gmail.com>
2018-04-19 12:24:17 +0530

    initial commit

---
+++ src/main/java/cmd/cmd1.java
@@ -0,0 +1,9 @@
+package cmd;
+
+public class cmd1 {
+	
+	public String sample() {
+		return "Sample Demo Java Class";
+	}
+}
+
---
+++ src/test/java/cmd/cmd2.java
@@ -0,0 +1,12 @@
+package cmd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class cmd2 {
+
+	@Test
+	public void test() {
+	}
+}
